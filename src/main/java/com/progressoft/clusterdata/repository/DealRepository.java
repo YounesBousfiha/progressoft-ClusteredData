046040490a188e86ad875490fddf7f5176055618
Coderyounes <younesanas2021@gmail.com>
2026-02-25 21:05:35 +0000

    add DealRepository interface extending JpaRepository with custom method

--- src/main/java/com/progressoft/clusterdata/repository/DealRepository.java
+++ src/main/java/com/progressoft/clusterdata/repository/DealRepository.java
@@ -1,4 +1,11 @@
 package com.progressoft.clusterdata.repository;
 
-public interface DealRepository {
+
+import com.progressoft.clusterdata.entity.Deal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DealRepository extends JpaRepository<Deal, Long> {
+    boolean existsByDealUniqueId(String dealUniqueId);
 }
