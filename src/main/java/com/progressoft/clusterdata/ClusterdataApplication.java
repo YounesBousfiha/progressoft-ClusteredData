e0a6ada3f1ab7e6550b4fd843f267f57b12df87f
Coderyounes <younesanas2021@gmail.com>
2026-02-25 23:33:40 +0000

    initialize Spring Boot application with configuration files and dependencies

---
+++ src/main/java/com/progressoft/clusterdata/ClusterdataApplication.java
@@ -0,0 +1,13 @@
+package com.progressoft.clusterdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClusterdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClusterdataApplication.class, args);
+	}
+
+}
