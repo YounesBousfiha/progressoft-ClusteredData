f3cdb054f28c018804ad029ae9d12a3c2baffffa
Coderyounes <younesanas2021@gmail.com>
2026-02-25 20:59:56 +0000

    add Deal entity with fields and JPA annotations

--- src/main/java/com/progressoft/clusterdata/entity/Deal.java
+++ src/main/java/com/progressoft/clusterdata/entity/Deal.java
@@ -1,4 +1,37 @@
 package com.progressoft.clusterdata.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "fx_deals")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Deal {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  long id;
+
+    @Column(name = "deal_unique_id", unique = true, nullable = false)
+    private String dealUniqueId;
+
+    @Column(name = "from_currency", nullable = false, length = 3)
+    private String fromCurrency;
+
+    @Column(name = "to_currency", nullable = false, length = 3)
+    private String toCurrency;
+
+    @Column(name = "deal_timestamp", nullable = false)
+    private LocalDateTime dealTimestamp;
+
+    @Column(name = "deal_amount", nullable = false)
+    private BigDecimal dealAmount;
 }
