277aa5d0759b3be0e6819b3f5cd2441a630a8490
Coderyounes <younesanas2021@gmail.com>
2026-02-25 21:45:51 +0000

    add custom query method to DealRepository for finding existing deal IDs

--- src/main/java/com/progressoft/clusterdata/repository/DealRepository.java
+++ src/main/java/com/progressoft/clusterdata/repository/DealRepository.java
@@ -3,9 +3,16 @@ package com.progressoft.clusterdata.repository;
 
 import com.progressoft.clusterdata.entity.Deal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Set;
 
 @Repository
 public interface DealRepository extends JpaRepository<Deal, Long> {
-    boolean existsByDealUniqueId(String dealUniqueId);
+
+    @Query("SELECT d.dealUniqueId FROM Deal d WHERE d.dealUniqueId IN :ids")
+    Set<String> findExistingDealIds(@Param("ids") List<String> ids);
 }
