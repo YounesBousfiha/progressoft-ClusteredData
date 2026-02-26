c3440da222a0a616d079959b3c0a1d6b46e0234e
Coderyounes <younesanas2021@gmail.com>
2026-02-26 10:27:25 +0000

    add unit test for ClusterdataApplication to verify application context loading

---
+++ src/test/java/com/progressoft/clusterdata/ClusterdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progressoft.clusterdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClusterdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
